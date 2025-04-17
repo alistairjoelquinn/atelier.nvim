dc4f7ba7373226c17ae971c6863ede7e4e6d142a
alistairjoelquinn <alistairjoelquinn@gmail.com>
2025-04-17 21:26:53 +0200

    refactor to default window to a table

--- lua/window.lua
+++ lua/window.lua
@@ -1,17 +1,19 @@
 local M = {}
 
-local window_data = nil
+local window_data = {}
 
 -- Map 'q' to close the window
 local create_keymaps = function()
   print 'creating keymaps'
-  vim.api.nvim_buf_set_keymap(
-    window_data.buf,
-    'n',
-    'q',
-    ':DynamicThemeClose<CR>',
-    { noremap = true, silent = true }
-  )
+  if window_data.buf then
+    vim.api.nvim_buf_set_keymap(
+      window_data.buf,
+      'n',
+      'q',
+      ':DynamicThemeClose<CR>',
+      { noremap = true, silent = true }
+    )
+  end
 end
 
 local create_window = function(opts)
@@ -42,10 +44,10 @@ local create_window = function(opts)
 end
 
 M.open_window = function(opts)
-  if window_data and vim.api.nvim_win_is_valid(window_data.win) then
+  if window_data.win and vim.api.nvim_win_is_valid(window_data.win) then
     print 'old window used'
     vim.api.nvim_set_current_win(window_data.win)
-    create_keymaps(window_data)
+    create_keymaps()
     return
   end
 
@@ -54,8 +56,9 @@ M.open_window = function(opts)
 end
 
 M.close_window = function()
-  if window_data and vim.api.nvim_win_is_valid(window_data.win) then
+  if window_data.win and vim.api.nvim_win_is_valid(window_data.win) then
     vim.api.nvim_win_close(window_data.win, true)
+    window_data = {}
   end
 end
 
