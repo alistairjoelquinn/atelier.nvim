c8582d315146830bb624c4029677a530dd9a6844
alistairjoelquinn <alistairjoelquinn@gmail.com>
2025-04-17 21:02:49 +0200

    adds commands for opening and closing the window

--- lua/dynamic-theme.lua
+++ lua/dynamic-theme.lua
@@ -3,6 +3,23 @@ local create_window = require 'create-window'
 local create_highlight_groups = require 'create-highlight-groups'
 
 local M = {}
+local window_data = nil
+
+M.open_window = function(opts)
+  if window_data and vim.api.nvim_win_is_valid(window_data.win) then
+    vim.api.nvim_set_current_win(window_data.win)
+    return
+  end
+
+  window_data = create_window(opts)
+end
+
+M.close_window = function()
+  if window_data and vim.api.nvim_win_is_valid(window_data.win) then
+    vim.api.nvim_win_close(window_data.win, true)
+    window_data = nil
+  end
+end
 
 M.setup = function(opts)
   opts = opts or {}
@@ -25,6 +42,15 @@ M.setup = function(opts)
   for group, settings in pairs(highlight_groups) do
     vim.api.nvim_set_hl(0, group, settings)
   end
+
+  -- Create commands for opening and closing the window
+  vim.api.nvim_create_user_command('DynamicThemeOpen', function()
+    M.open_window()
+  end, {})
+
+  vim.api.nvim_create_user_command('DynamicThemeClose', function()
+    M.close_window()
+  end, {})
 end
 
 return M
